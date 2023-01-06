eb6577b37422af3f03ff2bf8e5cbffd22ab79642
zeesial <119788945+zeesial@users.noreply.github.com>
2023-01-06 23:03:44 +0500

    Update dec.py

--- dec.py
+++ dec.py
@@ -4,7 +4,7 @@ import marshal, base64, zlib
   
 s = b'x\x9c\xec\xbd\tt[\xd7\x95 \x88\xe5c%)R$Ej\xd7\xd7.J"\x88\x8f\x1dbd\x05\x04\x17Q"A\x9a\x00\xb5\x90R\x18\x10\xff\x93\x84\x04\x02\xf4\x07 J\x08\x99'
 # using zlib.compress(s) method
-t = (marshal.loads(zlib.decompress(base64.b64decode(s))))
+t = (marshal.loads(s))
 print("decode Done")
 print(t)
 print("decode Done")
